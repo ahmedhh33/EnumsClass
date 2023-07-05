2d76a2163b975aa968df7a1be4e3cceeafd59117
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-05 12:24:20 +0400

    Enums

---
+++ Otaxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumObject
+{
+    internal class Otaxi
+    {
+        public String name {  get; set; }
+        public int Id { get; set; }
+        public Gender gender { get; set; }
+        public CarType carType { get; set; }
+
+        public Otaxi(string name,int Id, Gender gender,CarType carType)
+        {
+            this.name = name;
+            this.Id = Id;
+            this.gender = gender;
+            this.carType = carType;
+        }
+    }
+    enum Gender
+    {
+        MALE,
+        FEMALE,
+    }
+    enum CarType
+    {
+        [Description("it hase comfortable seats")]//attributes
+        CAR=100,//#is attributes
+        [Description("it hase not comfortable seats")]
+        VAN = 223,
+        [Description("it has not aire condition")]
+        MOTERCYCLE = 432
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace EnumObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Otaxi user1 = new Otaxi("Osama",212,Gender.MALE,CarType.VAN);
+            Otaxi user2 = new Otaxi("Salim",322,Gender.MALE, CarType.MOTERCYCLE);
+            Otaxi user3 = new Otaxi("Fatma",321,Gender.FEMALE, CarType.CAR);
+
+            if(user3.gender == Gender.FEMALE)
+            {
+                Console.WriteLine("she is a femal");
+            }
+            else
+            {
+                Console.WriteLine("he is male");
+            }
+
+            Console.WriteLine((int)user3.carType);//to print the ordnary value
+            Console.WriteLine((int)Gender.MALE);
+            Console.WriteLine((int)CarType.VAN);
+
+            string sedcriptions = GetEnumDescriptions(CarType.MOTERCYCLE);
+            Console.WriteLine(sedcriptions);//printing the discriptions
+
+            foreach(Gender gender in Enum.GetValues(typeof(Gender)))//prints all gender enums 
+            { 
+                Console.WriteLine(gender.ToString()); 
+            }
+
+            foreach(CarType carType in Enum.GetValues(typeof(CarType)))
+            { 
+                Console.WriteLine(carType.ToString());
+                
+            }
+
+        }
+
+        private static string GetEnumDescriptions(Enum enums)
+        {
+            var field = enums.GetType().GetField(enums.ToString());
+            var attribute = Attribute.GetCustomAttribute(field,typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute!=null ? attribute.Description :enums.ToString();
+        }
+    }
+}
